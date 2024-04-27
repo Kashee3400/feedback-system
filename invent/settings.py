455d77727feb26d8dcafd91e16d864332568f497
Kashee3400 <kasheemilkpcl@gmail.com>
2024-04-27 12:57:10 +0530

    Updated

--- invent/settings.py
+++ invent/settings.py
@@ -202,9 +202,9 @@ MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 
 STATIC_URL="/static/"
-STATIC_DIR = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [STATIC_DIR]
+# STATICFILES_DIRS = [STATIC_DIR]
 
 
 # Default primary key field type
